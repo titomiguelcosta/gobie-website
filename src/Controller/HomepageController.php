ef011641d8a47b76812e2f30c7941ccfd4f9590f
Tito Miguel Costa <titomiguelcosta@gmail.com>
2021-12-24 15:53:04 +0100

    added mailer

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -8,13 +8,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class HomepageController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function __invoke(Request $request, Security $security, \Swift_Mailer $mailer)
+    public function __invoke(Request $request, Security $security, MailerInterface $mailer)
     {
         if ($security->getUser() instanceof User) {
             return $this->redirectToRoute('project_list');
@@ -26,14 +28,12 @@ class HomepageController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
 
-            $message = (new \Swift_Message('Gobie: Early Access Request'))
-                ->setFrom('gobie@titomiguelcosta.com')
-                ->setTo($data['email'])
-                ->setBcc('gobie@titomiguelcosta.com')
-                ->setBody(
-                    'Hello, thanks for your interest. We got your request. In the upcoming assembly, the chimps will decide if we are ready to welcome you. Bear with us.',
-                    'text/plain'
-                );
+            $message = (new Email())
+                ->subject('Gobie: Early Access Request')
+                ->from('gobie@titomiguelcosta.com')
+                ->to($data['email'])
+                ->bcc('gobie@titomiguelcosta.com')
+                ->text('Hello, thanks for your interest. We got your request. In the upcoming assembly, the chimps will decide if we are ready to welcome you. Bear with us.');
 
             $mailer->send($message);
 
